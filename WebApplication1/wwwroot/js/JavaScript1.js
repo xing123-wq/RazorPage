1b1c232ebef6920a8131ddb576d5697cc5797eb3
龙泰 幸 <1978174217@qq.com>
2019-09-18 15:01:23 +0800

    完成铃铛闪烁

---
+++ WebApplication1/wwwroot/js/JavaScript1.js
@@ -0,0 +1,10 @@
+﻿//铃铛闪烁
+function changecolor() {
+    var star = document.getElementById('greet');
+    if (star.style.color === 'red') {
+        star.style.color = 'green';
+    } else {
+        star.style.color = 'red';
+    }
+}
+setInterval(changecolor, 1000);
