4f8ecfd163f5be940e4438a82a9d4682d9ae3e5a
龙泰 幸 <1978174217@qq.com>
2019-09-24 16:56:42 +0800

    实现递增

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,3 +1,20 @@
-﻿window.onbeforeunload = function (e) {
-    return '';
+﻿var parssword = document.querySelectorAll('[xlt]');
+var verifypassword = document.querySelectorAll('[xlt]');
+
+if (parssword === verifypassword) {
+
+}
+else {
+    alert('你输入的密码不一致');
+}
+//window.onbeforeunload = function (e) {
+//    return '';
+//};      
+document.getElementsByTagName('form')[0].onsubmit = function () {
+    if (document.getElementsByName('UserName')[0].value === '') {
+        return false;
+    }
+}
+window.onbeforeunload = function (e) {
+    return '关闭提示';
 };
