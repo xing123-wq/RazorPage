c35fe22f9683411d5a5b055c0c40fd709c5337f3
龙泰 幸 <1978174217@qq.com>
2019-09-26 22:41:25 +0800

    完成查询页面有多少个悬赏，

--- WebApplication1/wwwroot/js/JavaScript3.js
+++ WebApplication1/wwwroot/js/JavaScript3.js
@@ -1,11 +1,9 @@
 ﻿var offer = document.getElementsByClassName('money');
 var reward = 0;
 for (var i = 0; i < offer.length; i++) {
-    if (offer[i] > 0) {
+    
         reward++;
-    } else {
-
-    }
+    
 }
-alert(reward);
+alert(reward);//把js文件放在下面，后运行，把js文件放在上面，先运行
 
