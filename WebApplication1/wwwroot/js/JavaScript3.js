4376413cf233d85621eb6a223856397ac6248432
龙泰 幸 <1978174217@qq.com>
2019-10-09 21:42:55 +0800

    还没有完成点击按钮输入框没有值显示下面的提示，有值隐藏该输入框下面的提示

--- WebApplication1/wwwroot/js/JavaScript3.js
+++ WebApplication1/wwwroot/js/JavaScript3.js
@@ -1,8 +1,1 @@
-﻿var offer = document.getElementsByClassName('money');
-var reward = 0;
-for (var i = 0; i < offer.length; i++) {
-
-    reward++;
-
-}
-alert(reward);//把js文件放在下面，后运行，把js文件放在上面，先运行
+﻿
