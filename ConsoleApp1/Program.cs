7fe946e01d3576e6ccd69e3a5232abd30d3f494f
龙泰 幸 <1978174217@qq.com>
2019-11-11 23:03:24 +0800

    完成最大值，最小值

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -71,15 +71,94 @@ namespace ConsoleApp1
             按进栈时间装入一维数组，
             按座位装入二维数组，
             并输出共有多少名同学。/*/
-            string[] names = new string[] { "阿泰", "与围墙", "清哥", "王总" };
+            string[] names = new string[] { "阿泰", "与围墙", "清哥", "王总", "彭总", "陈百万" };
             Console.WriteLine(names.Length);
-            string[,] array = new string[2, 3] { { "阿泰", "与围墙", "清哥" }, { "王总", "", "" } };
+            string[,] array = new string[2, 3] { { "幸龙泰", "于维谦", "曾俊清" }, { "王新", "彭志强", "陈百万" } };
             Console.WriteLine(array.Length);
 
             // 电脑计算并输出：[(23 + 7)x12-8]÷6的小数值（精确到小数点以后x位）
             Console.WriteLine((Math.Round(((23 + 7) * 12 - 8) / (float)6, 2)));
 
 
+            //分别用for循环和while循环输出：1,2,3,4,5 和 1,3,5,7,9
+            //int i;
+            Console.WriteLine("for循环：");
+            for (i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("while循环：");
+            int q = 0;
+            while (q < 5)
+            {
+                q++;
+                Console.WriteLine(q);
+            }
+            Console.WriteLine("for循环:");
+            int t;
+            for (t = 1; t < 10; t++)
+            {
+                if (t % 2 != 0)
+                {
+                    Console.WriteLine(t);
+                }
+            }
+            Console.WriteLine("while循环:");
+            int w = 0;
+            while (w < 10)
+            {
+                if (w % 2 != 0)
+                {
+                    Console.WriteLine(w);
+                }
+                w++;
+            }
+            //用for循环输出存储在一维 / 二维数组里的源栈所有同学姓名 / 昵称
+            Console.WriteLine("一维数组：");
+            for (i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.WriteLine("二维数组：");
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(array[i, j]);
+
+                }
+            }
+            //让电脑计算并输出：99 + 97 + 95 + 93 + ...+1的值
+            int sum = 0;
+            for (i = 1; i < 100; i += 2)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+
+            //将源栈同学的成绩存入一个double数组中，用循环找到最高分和最低分
+            double[] aj = { 10, 9, 0.6, 42.34, 312 };
+            double max = aj[0], min = aj[0];
+            Console.WriteLine("最大值：");
+            for (i = 0; i < aj.Length; i++)
+            {
+                if (max < aj[i])
+                {
+                    max = aj[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine("最小值：");
+            for (i = 0; i < aj.Length; i++)
+            {
+                if (min > aj[i])
+                {
+                    min = aj[i];
+                }
+            }
+            Console.WriteLine(min);
+            //找到100以内的所有质数（只能被1和它自己整除的数）
+            //test1:
             Console.ReadLine();
 
         }
