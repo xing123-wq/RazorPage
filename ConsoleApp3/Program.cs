70ad92d2ea4b154629742e2e754adae2fecb6b5c
龙泰 幸 <1978174217@qq.com>
2019-12-03 21:57:06 +0800

    实现输入任意年份打印若干天的日期

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -28,6 +28,34 @@ namespace ConsoleApp3
             Console.WriteLine(friend.Student);
             atai.Student = "王新";
             Console.WriteLine(friend.Student);
+
+            GetWeeks(2019/1/1);
+        }
+        //源栈的学费是按周计费的，所以请实现这两个功能：
+        //函数GetDate()，能计算一个日期若干（日 / 周 / 月）后的日期
+        //给定任意一个年份，就能按周排列显示每周的起始日期，如下图所示：
+        static void GetWeeks(int year)
+        {
+            DateTime date = GetFirstModay(year);
+            DateTime LastMonday = GetFirstModay(year + 1).AddDays(-7);
+            while (date < LastMonday)
+            {
+                Console.WriteLine(date.ToString("yyyy年MM月dd日"));
+                date = date.AddDays(6);
+                Console.WriteLine("-");
+                Console.WriteLine(date.ToString("yyyy年MM月dd日"));
+                date = date.AddDays(1);
+                Console.WriteLine();
+            }
+        }
+        private static DateTime GetFirstModay(int year)
+        {
+            DateTime date = new DateTime(year, 1, 1);
+            while (date.DayOfWeek != DayOfWeek.Monday)
+            {
+                date = date.AddDays(1);
+            }
+            return date;
         }
     }
 }
