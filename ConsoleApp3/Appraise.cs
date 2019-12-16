f7fc4d8c20d333b9dd58c1af1b3356baa25735f0
龙泰 幸 <1978174217@qq.com>
2019-12-16 11:27:36 +0800

    给一起帮关键字设置索引器

---
+++ ConsoleApp3/Appraise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Appraise
+    {
+        Content Iarget { get; set; }
+        User Wotter { get; set; }
+        bool AgreeOrNot { get; set; }
+        void Agree()
+        {
+
+        }
+        void Disagree()
+        {
+
+        }
+    }
+}
