d86df5f20d5378dcf11059276995f6e7cbf35e77
龙泰 幸 <1978174217@qq.com>
2019-12-22 20:20:17 +0800

    注释XML代码

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -39,38 +39,38 @@ namespace ConsoleApp3
 
             LinqWork.Do();
             //按以下格式生成一个XML对象：
-            XElement articles = new XElement(
-               "articles",
-               new XComment("以下存放所有“源栈”所有文章"),
-               new XElement("article",
-                   new XAttribute("isDraft", false),
-                   new XElement("id", "1"),
-                   new XElement("title", "源栈培训：C#进阶-7：Linq to XML"),
-                   new XElement("body", "什么是XML（EXtensible Markup Language）" +
-                   "是一种文本文件格式被设计用来传输和存储数据由：标签和属性组成元素（节点），由元素组成“树状结构”必须有而且只能有一个根节点其他："),
-                   new XElement("authorId", "1"),
-                   new XElement("publishDate", "2019/6/18 18:15"),
-                   new XElement("comments",
-                   new XElement("comment",
-                   new XElement("id", "12"),
-                   new XElement("body", "不错，赞！"),
-                   new XElement("authorId", "2"),
-                   new XAttribute("recommend", true)),
-                   new XElement("comment",
-                   new XElement("id", "14"),
-                   new XElement("body", "作业太难了"),
-                   new XElement("authorId", "3")))),
-               new XElement("article",
-                   new XElement("id", "2"),
-                   new XElement("title", "源栈培训：C#进阶-6：异步和并行"),
-                   new XElement("authorId", 1),
-                   new XAttribute("isDraft", true))
-               );
-            Console.WriteLine(articles);
-            XDocument document = new XDocument(
-               new XDeclaration("1.0", "utf-8", "yes"),   //添加一个XML声明
-               articles);
-            document.Save("F:\\17bang\\articles.xml");
+            //XElement articles = new XElement(
+            //   "articles",
+            //   new XComment("以下存放所有“源栈”所有文章"),
+            //   new XElement("article",
+            //       new XAttribute("isDraft", false),
+            //       new XElement("id", "1"),
+            //       new XElement("title", "源栈培训：C#进阶-7：Linq to XML"),
+            //       new XElement("body", "什么是XML（EXtensible Markup Language）" +
+            //       "是一种文本文件格式被设计用来传输和存储数据由：标签和属性组成元素（节点），由元素组成“树状结构”必须有而且只能有一个根节点其他："),
+            //       new XElement("authorId", "1"),
+            //       new XElement("publishDate", "2019/6/18 18:15"),
+            //       new XElement("comments",
+            //       new XElement("comment",
+            //       new XElement("id", "12"),
+            //       new XElement("body", "不错，赞！"),
+            //       new XElement("authorId", "2"),
+            //       new XAttribute("recommend", true)),
+            //       new XElement("comment",
+            //       new XElement("id", "14"),
+            //       new XElement("body", "作业太难了"),
+            //       new XElement("authorId", "3")))),
+            //   new XElement("article",
+            //       new XElement("id", "2"),
+            //       new XElement("title", "源栈培训：C#进阶-6：异步和并行"),
+            //       new XElement("authorId", 1),
+            //       new XAttribute("isDraft", true))
+            //   );
+            //Console.WriteLine(articles);
+            //XDocument document = new XDocument(
+            //   new XDeclaration("1.0", "utf-8", "yes"),   //添加一个XML声明
+            //   articles);
+            //document.Save("F:\\17bang\\articles.xml");
         }
         //源栈的学费是按周计费的，所以请实现这两个功能：
         //函数GetDate()，能计算一个日期若干（日 / 周 / 月）后的日期
