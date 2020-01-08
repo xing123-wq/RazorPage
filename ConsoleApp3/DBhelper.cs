33c18de68feb52ea78338e71ca6fe8bb290de18e
龙泰 幸 <1978174217@qq.com>
2020-01-08 11:37:57 +0800

    创建DBhelper类,建立数据库连接

---
+++ ConsoleApp3/DBhelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ConsoleApp3
+{
+    class DBhelper
+    {
+        //构建一个DBHelper，可以提供：
+        //封装过后的ExecuteNonQuery()/ExecuteScalar()/ExecuteReader()方法
+        //由外部控制开闭（Open()/Close()）的长连接（LongConnection）
+        //ExecuteNonQuery()使用长连接的重载
+
+        //使用上述DBHelper，完成以下操作：
+        //将用户名和密码存入数据库：Register()
+        //根据用户名和密码检查某用户能够成功登陆：Logon()
+        //如果用户成功登陆，将其最后登录时间（LatestLogonTime）改成当前时间
+        //批量标记Message为已读
+        static void SQL()
+        {
+            string connectionString = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();  //需要显式的Open()
+                                    //进行其他操作
+            }
+        }
+    }
+}
