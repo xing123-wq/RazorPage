2cf54c1eb4cc60b13bc214503711f15d938ae515
龙泰 幸 <1978174217@qq.com>
2020-01-09 11:28:50 +0800

    创建数据库连接. 给数据库插入数据

--- ConsoleApp3/DBhelper.cs
+++ ConsoleApp3/DBhelper.cs
@@ -17,13 +17,24 @@ namespace ConsoleApp3
         //根据用户名和密码检查某用户能够成功登陆：Logon()
         //如果用户成功登陆，将其最后登录时间（LatestLogonTime）改成当前时间
         //批量标记Message为已读
-        static void SQL()
+        public static void SQL()
         {
-            string connectionString = null;
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                    Initial Catalog=17bang;
+                                    Integrated Security=True;
+                                    Connect Timeout=30;
+                                    Encrypt=False;
+                                    TrustServerCertificate=False;
+                                    ApplicationIntent=ReadWrite;
+                                    MultiSubnetFailover=False";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();  //需要显式的Open()
-                                    //进行其他操作
+                SqlCommand command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = @"INSERT DREAM VALUES(N'与围墙',11)";
+                int row = command.ExecuteNonQuery();
+
             }
         }
     }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -41,7 +41,9 @@ namespace ConsoleApp3
 
             //XML.Do();
 
-            Repoistory.Do();
+            //Repoistory.Do();
+
+            DBhelper.SQL();
         }
         //源栈的学费是按周计费的，所以请实现这两个功能：
         //函数GetDate()，能计算一个日期若干（日 / 周 / 月）后的日期
