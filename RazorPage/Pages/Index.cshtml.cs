e5d43daf80fe02befbb91ae14a38155bb66a9bf9
龙泰 幸 <1978174217@qq.com>
2020-01-16 20:54:49 +0800

    瞎提交

---
+++ RazorPage/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPage.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
