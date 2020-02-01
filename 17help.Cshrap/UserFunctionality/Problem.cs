0411872ab85e884bbf6ccdfaaecb638cd9724bbd
龙泰 幸 <1978174217@qq.com>
2020-02-01 14:12:00 +0800

    整理Problem类

--- 17help.Cshrap/UserFunctionality/Problem.cs
+++ 17help.Cshrap/UserFunctionality/Problem.cs
@@ -4,25 +4,15 @@ using System.Text;
 
 namespace ConsoleApp3
 {
-    internal class Problem : Content
+    public class Problem : Content
     {
         //一起帮的求助可以有多个（最多10个）关键字，
         //请为其设置索引器，以便于我们通过其整数下标进行读写。
-        public string[] Keyword = {
-            "C#", "编程语言",
-            "JavaScript", "工具软件",
-            "Java", "顾问咨询", "SQL",
-            "职场", "法律", ".net"
-        };
-        public string this[int Index]
-        {
-            get { return Keyword[Index]; }
-            set { Keyword[Index] = value; }
-        }
-        //public Problem(string kind) : base(kind) { }
         internal string Title { get; set; }
         public string Body { get; set; }
+        public DateTime PublishDateTime { get; set; }
         public int _reward;
+        private readonly static ProblemRepoistory _repoistory;
         public int Reward
         {
             get { return _reward; }
@@ -38,7 +28,19 @@ namespace ConsoleApp3
                 }
             }
         }
-        public DateTime PublishDateTime { get; set; }
+
+        public string[] Keyword = {
+            "C#", "编程语言",
+            "JavaScript", "工具软件",
+            "Java", "顾问咨询", "SQL",
+            "职场", "法律", ".net"
+        };
+        public string this[int Index]
+        {
+            get { return Keyword[Index]; }
+            set { Keyword[Index] = value; }
+        }
+        //public Problem(string kind) : base(kind) { }
         public void Publish()
         {
             Author.credit++;
@@ -52,7 +54,6 @@ namespace ConsoleApp3
         {
 
         }
-        private readonly static ProblemRepoistory _repoistory;
 
 
     }
