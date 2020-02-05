3c1eada4924458d4bc622e2dfb042c67711bdb23
龙泰 幸 <1978174217@qq.com>
2020-02-05 15:17:32 +0800

    让IsFemale从前台获得值

---
+++ RazorPage/Pages/Profiel.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage
+{
+    [BindProperties]
+    public class ProfielModel : PageModel
+    {
+        public bool? IsFemale { get; set; }
+        public void OnGet()
+        {
+            ViewData["title"] = "个人资料--一起帮";
+        }
+        public void OnPost()
+        {
+
+        }
+    }
+}
