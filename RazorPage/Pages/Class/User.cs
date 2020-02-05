00e45d752215deb8b0274d8fe269ab692210e6e1
龙泰 幸 <1978174217@qq.com>
2020-02-05 20:45:59 +0800

    注册页面,注册呈现提示

--- RazorPage/Pages/Class/User.cs
+++ RazorPage/Pages/Class/User.cs
@@ -1,11 +1,19 @@
-﻿namespace RazorPage
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPage
 {
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "*用户名不能为空")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "*密码不能为空")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "*确认密码不能为空")]
         public string ValidatePassword { get; set; }
-       
+        [Required(ErrorMessage = "*邀请人不能为空")]
+        public string inviter { get; set; }
+        [Required(ErrorMessage = "*邀请码不能为空")]
+        public int Invitationcode { get; set; }
     }
 }
