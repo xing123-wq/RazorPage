bf814a8466fe40db5dd8cfbd611a395578425a2a
龙泰 幸 <1978174217@qq.com>
2020-02-07 15:40:25 +0800

    引用的新文件

---
+++ RazorPage/Pages/Problems/New.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPage
+{
+    public class NewsModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["title"] = "(新消息)我要求助--一起帮";
+        }
+    }
+}
