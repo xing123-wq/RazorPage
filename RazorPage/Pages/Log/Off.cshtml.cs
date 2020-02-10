5c5599acea75778bc918fc150f9546d1c1d72c1b
龙泰 幸 <1978174217@qq.com>
2020-02-10 20:13:33 +0800

    注册页面能够提示用户已存在

---
+++ RazorPage/Pages/Log/Off.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPage.Pages.Helper;
+
+namespace RazorPage
+{
+    public class OffModel : _LayoutModel
+    {
+        public ActionResult OnGet()
+        {
+            Response.Cookies.Delete(Const.USER_ID);
+            Response.Cookies.Delete(Const.USER_PASSWORD);
+            base.SetLogOnStatus();
+            return RedirectToPage("/Log/On");
+        }
+    }
+}
