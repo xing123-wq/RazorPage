6c0571113a6c470dac9f8bac71d182e37bf21fc1
龙泰 幸 <1978174217@qq.com>
2020-02-28 17:26:59 +0800

    删除不必要的代码

--- 17bangMvc/Controllers/HomeController.cs
+++ 17bangMvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _17bangMvc.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,21 +9,24 @@ namespace _17bangMvc.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            return View();
+        }
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
