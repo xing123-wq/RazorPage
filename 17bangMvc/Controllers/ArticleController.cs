a12d1dbe81a8647beb83dff1125ea4c5664935a1
龙泰 幸 <1978174217@qq.com>
2020-02-28 23:42:31 +0800

    添加

---
+++ 17bangMvc/Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using _17bangMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _17bangMvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+        [HttpGet]
+        public ActionResult index()
+        {
+            ViewData["title"] = "精品文章-一起帮";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult index(ArticleModel model)
+        {
+            return View();
+        }
+    }
+}
