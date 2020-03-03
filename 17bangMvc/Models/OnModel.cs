54f8680984c3caa0c91e7a100b06d2c1bb10b503
龙泰 幸 <1978174217@qq.com>
2020-03-03 22:01:20 +0800

    给属性添加验证

--- 17bangMvc/Models/OnModel.cs
+++ 17bangMvc/Models/OnModel.cs
@@ -9,11 +9,21 @@ namespace _17bangMvc.Models
     public class OnModel
     {
         public int Id { get; set; }
-        [Required]
+
+        [Display(Name = "用户名：（* 必填）")]
+        [Required(ErrorMessage = "* 用户名不能为空")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "* 用户名长度不能小于{2}也不能大于{1}")]
         public string Name { get; set; }
+
+        [Display(Name = "密码：（* 必填）")]
+        [Required(ErrorMessage = "* 密码不能为空")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "* 密码必须在{2} 和{1}之间")]
         public string Password { get; set; }
-        public bool RememberMe { get; set; }
+
+        [Display(Name = "验证码:")]
+        [Required(ErrorMessage = "* 验证码不能为空")]
+        [MinLength(4, ErrorMessage = "* 验证码长度只能等于4位")]
         public string SecurityCode { get; set; }
-        public DateTime CreatingTime { get; set; }
+        public bool RememberMe { get; set; }//记住我
     }
 }
