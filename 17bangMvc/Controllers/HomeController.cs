f0b68ee01f4cb8da70ba5f54915696aca9ad682c
龙泰 幸 <1978174217@qq.com>
2020-03-05 20:31:01 +0800

    调整

--- 17bangMvc/Controllers/HomeController.cs
+++ 17bangMvc/Controllers/HomeController.cs
@@ -12,20 +12,8 @@ namespace _17bangMvc.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            string name;
             ViewData["title"] = "一起帮-首页";
-            //Session里面有值，就表示已经登录
-            if (Session["User"] != null)
-            {
-                name = Session["User"].ToString();
-            }
-            //Session=null就跳转到登录页面
-            else
-            {
-                return RedirectToAction("Login", "Login");
-            }
-            return Content("欢迎【" + name + "】登录");
-
+            return View();
         }
         [HttpPost]
         public ActionResult Index(IndexModel model)
