aba7acc54a608e0f2ade86533be933909a370633
龙泰 幸 <1978174217@qq.com>
2020-03-07 19:36:29 +0800

    完成用合适的变量类型存储页面用户输入信息

---
+++ ConsoleApp1/Keword.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1
+{
+    internal class Keword
+    {
+        int Id;
+        string Name;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///个人资料的变量存储信息
+            string HeadPortrait;
+            ///头像获取的是本地图片的路径，然后进行上传
+            ///我觉得可以用bytep[]字节流来保存
+            bool IsFemale;
+            ///选择男true,选择女fales。
+            ///这样容易if,eles判断
+            int BirthYear;
+            ///因为年龄是整数
+            string Constellation;
+            string keyword;
+            ///这个该用keyword类型
+            ///因为可以在keyword类中通过id得到
+            string SelfDescription;
+        }
+    }
+}
