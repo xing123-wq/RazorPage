d24adce48cf37628966830c10bdbec5607d316fb
龙泰 幸 <1978174217@qq.com>
2020-03-08 11:13:58 +0800

    分别用for循环和while循环输出：1,2,3,4,5 和 1,3,5,7,9

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,94 +6,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            /////个人资料的变量存储信息
-            //string HeadPortrait;
-            /////头像获取的是本地图片的路径，然后进行上传
-            /////我觉得可以用bytep[]字节流来保存
-            //bool IsFemale;
-            /////选择男true,选择女fales。
-            /////这样容易if,eles判断
-            //int BirthYear;
-            /////因为年龄是整数
-            //string Constellation;
-            //string keyword;
-            /////这个该用keyword类型
-            /////因为可以在keyword类中通过id得到
-            //string SelfDescription;
-
-            //输出两个整数 / 小数的和 / 差 / 积 / 商
-            //int i = 23;
-            //int j = 33;
-            //Console.WriteLine(i + j);
-            //Console.WriteLine(i - j);
-            //Console.WriteLine(i * j);
-            //Console.WriteLine(i / j);
-            //double a = 2.7;
-            //double b = 0.3;
-            //Console.WriteLine(a + b);
-            //Console.WriteLine(a - b);
-            //Console.WriteLine(a * b);
-            //Console.WriteLine(a / b);
-
-            //电脑计算并输出：[(23 + 7)x12-8]÷6的小数值（挑战：精确到小数点以后2位）
-            //Console.WriteLine((Math.Round(((23 + 7) * 12 - 8) / (float)6, 2)));
-
-            ////当a为何值时，结果为true？
-            //int a = 10;
-            //bool result = (a + 3 > 12) && a < 3.14 * 4 && a != 11;
-            //Console.WriteLine(result);
-
-            //用户依次由控制台输入：验证码、用户名和密码：
-            //如果验证码输入错误，直接输出：“*验证码错误”；
-            //如果用户名不存在，直接输出：“*用户名不存在”；
-            //如果用户名或密码错误，输出：“*用户名或密码错误”
-            //以上全部正确无误，输出：“恭喜！登录成功！”
-            //string input = Console.ReadLine();
-            //if ("qwe1" == input)
+            //分别用for循环和while循环输出：1,2,3,4,5 和 1,3,5,7,9
+            //用for循环输出存储在一维 / 二维数组里的源栈所有同学姓名 / 昵称
+            //让电脑计算并输出：99 + 97 + 95 + 93 + ...+1的值
+            //将源栈同学的成绩存入一个double数组中，用循环找到最高分和最低分
+            //找到100以内的所有质数（只能被1和它自己整除的数）
+            //在一个整数数组中找到最大值
+            //Console.WriteLine("for循环：");
+            //for (int i = 1; i <= 5; i++)
             //{
-            //    Console.WriteLine("请输入用户名");
-            //    string inputs = Console.ReadLine();
-            //    if ("wf23" == inputs)
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("while循环：");
+            //int q = 0;
+            //while (q < 5)
+            //{
+            //    q++;
+            //    Console.WriteLine(q);
+            //}
+            //Console.WriteLine("for循环:");
+            //int t;
+            //for (t = 1; t < 10; t++)
+            //{
+            //    if (t % 2 != 0)
             //    {
-            //        Console.WriteLine("请输入密码");
-            //        string inputPassword = Console.ReadLine();
-            //        if ("1234" == inputPassword)
-            //        {
-            //            Console.WriteLine("恭喜！登录成功！");
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("*用户名或密码错误");
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("*用户名不存在");
+            //        Console.WriteLine(t);
             //    }
             //}
-            //else
+            //Console.WriteLine("while循环:");
+            //int w = 0;
+            //while (w < 10)
             //{
-            //    Console.WriteLine("*验证码错误");
+            //    if (w % 2 != 0)
+            //    {
+            //        Console.WriteLine(w);
+            //    }
+            //    w++;
             //}
-            //将源栈同学姓名 / 昵称分别：
-            //按进栈时间装入一维数组，
-            //按座位装入二维数组，
-            //并输出共有多少名同学。
-            string[] names = new string[] { "阿泰", "与围墙", "清哥", "王总", "彭总", "陈百万" };
-            for (int i = 0; i < names.Length; i++)
-            {
-                Console.WriteLine(names[i]);
-            }
-            Console.WriteLine(names.Length);
-            string[,] array = new string[2, 3] { { "幸龙泰", "于维谦", "曾俊清" }, { "王新", "彭志强", "陈百万" } };
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.WriteLine(array[i, j]);
-                }
-            }
-            Console.WriteLine(array.Length);
+
+
         }
 
     }
